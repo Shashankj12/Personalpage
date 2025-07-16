46a4ef1917c03e3187d2e5e03972de87137dfe67
shajadh <shashank.jadhav@tuhh.de>
2025-07-16 15:13:48 +0200

    small changes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// Placeholder for future interactivity
-console.log("Portfolio loaded!");
+function loadHTML(targetId, file) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(targetId).innerHTML = data;
+    })
+    .catch(err => console.error(`Error loading ${file}:`, err));
+}
+
+window.onload = () => {
+  loadHTML("education-content", "education.html");
+  loadHTML("experience-content", "experience.html");
+};
 
